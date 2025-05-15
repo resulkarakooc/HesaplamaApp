0c557aef2ef42e1144f09940439a17d54ce1e569
Resul Karakoç <118927761+resulkarakooc@users.noreply.github.com>
2025-05-15 17:07:48 +0300

    Create HesaplamaTest.java

---
+++ test/HesaplamaTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import src.Hesaplama;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HesaplamaTest {
+
+    Hesaplama h = new Hesaplama();
+
+    @Test
+    public void testTopla() {
+        assertEquals(7, h.topla(3, 4));
+    }
+
+    @Test
+    public void testCikar() {
+        assertEquals(2, h.cikar(5, 3));
+    }
+
+    @Test
+    public void testCarp() {
+        assertEquals(15, h.carp(3, 5));
+    }
+
+    @Test
+    public void testBol() {
+        assertEquals(2, h.bol(6, 3));
+    }
+
+    @Test
+    public void testBolSifir() {
+        assertThrows(ArithmeticException.class, () -> h.bol(4, 0));
+    }
+}
