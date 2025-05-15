49a107b3875edc1f0eb7c2f769128309e9778582
Resul Karakoç <118927761+resulkarakooc@users.noreply.github.com>
2025-05-15 17:06:45 +0300

    Create Hesaplama.java

---
+++ src/Hesaplama.java
@@ -0,0 +1,23 @@
+package src;
+
+public class Hesaplama {
+
+    public int topla(int a, int b) {
+        return a + b;
+    }
+
+    public int cikar(int a, int b) {
+        return a - b;
+    }
+
+    public int carp(int a, int b) {
+        return a * b;
+    }
+
+    public int bol(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Bölen 0 olamaz.");
+        }
+        return a / b;
+    }
+}
